3b5d74b40dc3ac0c72f781401a41f1b13c9d708f
Tim Geyssens <tg@umbraco.com>
2013-07-19 15:05:33 +0200

    WIP of ajax style locator

---
+++ GeoCodingTest/Umbraco/LocatorApiController.cs
@@ -0,0 +1,74 @@
+﻿using Locator.GeoCoding;
+using Locator.GeoCoding.Services.Google;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.WebApi;
+
+namespace Locator.Umbraco
+{
+    public class LocatorApiController : UmbracoApiController
+    {
+
+        public IEnumerable<ExpandoObject> GetNearest(string address, int parentId, string docTypeFilter, string locationPropAlias, int numberOfSearchResults)
+        {
+            UmbracoHelper help = new UmbracoHelper(UmbracoContext);
+
+            var nodesToSearchThrough = help.TypedContent(parentId)
+                .Children
+                .Where(c => c.DocumentTypeAlias == docTypeFilter).Where("Visible");
+
+            var distanceunit = DistanceUnits.Kilometers;
+
+            var items = new List<GeoItem>();
+            var searchLocation = new Location();
+
+            GeoResponse r = GoogleGeoCoder.CallGeoWS(address);
+
+            searchLocation = new Location(r.Results[0].Geometry.Location.Lat, r.Results[0].Geometry.Location.Lng);
+
+
+            foreach (var node in nodesToSearchThrough) {
+                         
+              Location itemLocation = new Location(
+                    Convert.ToDouble(node.GetProperty(locationPropAlias).Value.ToString().Split(',')[0], Utility.NumberFormatInfo),
+                    Convert.ToDouble(node.GetProperty(locationPropAlias).Value.ToString().Split(',')[1], Utility.NumberFormatInfo));  
+              
+              items.Add(new GeoItem(node,itemLocation,searchLocation.DistanceBetween(itemLocation,distanceunit)));                                                                           
+           }
+
+            items.Sort(new GeoItemComparer());
+
+            var retval = new List<ExpandoObject>();
+            foreach (GeoItem geo in items.Take(numberOfSearchResults))
+            {
+                IPublishedContent node = ((IPublishedContent)geo.Node);
+
+                var obj = new ExpandoObject() as IDictionary<string, Object>;
+
+                obj.Add("Distance", geo.Distance);
+                obj.Add("Latitude", geo.Location.Latitude);
+                obj.Add("Longitude", geo.Location.Longitude);
+
+                obj.Add("Name", node.Name);
+                obj.Add("Url", node.Url);
+
+                foreach (var nodeProp in node.Properties)
+                {
+                    obj.Add(nodeProp.Alias, nodeProp.Value);
+                }
+
+                retval.Add((ExpandoObject)obj);
+            }
+            return retval;
+        }
+
+    }
+}
